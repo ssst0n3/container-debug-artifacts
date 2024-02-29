b3e838e75ecdcefc8c1458dd330a217ae101b493
LEI WANG <ssst0n3@gmail.com>
2024-02-29 16:46:27 +0800

    update
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- runc/v1.1.11/image.sh
+++ runc/v1.1.11/image.sh
@@ -3,7 +3,7 @@
 if [[ -d "runc" ]]; then
   echo "[+] The directory 'runc' exists."
 else
-    git clone --depth 1 --branch v1.1.0 https://github.com/opencontainers/runc.git
+    git clone --depth 1 --branch v1.1.11 https://github.com/opencontainers/runc.git
 fi
 
 ./apply-patch.sh
