34cbb48d0b2eb6a2d9b55f641fbb7012785395b6
LEI WANG <ssst0n3@gmail.com>
2024-03-07 10:04:50 +0800

    runc v1.1.10: init
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

---
+++ runc/v1.1.10/apply-patch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cwd=$PWD
+src=$cwd/runc
+
+series=$cwd/series.conf
+while IPF= read -r line
+do
+    if [[ "$line" =~ ^patch* ]]; then
+        echo patch -p1 $cwd/$line
+ 	    cd $src && patch -p1 < $cwd/$line
+    fi
+done <"$series"
