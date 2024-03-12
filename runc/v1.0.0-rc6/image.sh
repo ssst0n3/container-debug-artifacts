62ec07a7b1b009f8d8637dbe19e8bd21c2a7156b
LEI WANG <ssst0n3@gmail.com>
2024-03-12 20:03:32 +0800

    runc v1.0.0-rc6: init
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

---
+++ runc/v1.0.0-rc6/image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ -d "runc" ]]; then
+  echo "[+] The directory 'runc' exists."
+else
+    git clone --depth 1 --branch v1.0.0-rc6 https://github.com/opencontainers/runc.git
+fi
+
+./apply-patch.sh
+cd runc
+make runcimage
