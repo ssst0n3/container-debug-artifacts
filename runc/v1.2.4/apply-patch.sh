ec0d72f9372623685a0b1496ff80eb7aca29beb5
LEI WANG <ssst0n3@gmail.com>
2025-04-09 20:10:50 +0800

    add a comment to hint, do not commit after the patch
    Signed-off-by: LEI WANG <ssst0n3@gmail.com>

--- runc/v1.2.4/apply-patch.sh
+++ runc/v1.2.4/apply-patch.sh
@@ -9,5 +9,7 @@ do
     if [[ "$line" =~ ^patch* ]]; then
         echo patch -p1 $cwd/$line
  	    cd $src && patch -p1 < $cwd/$line
+        # do not commit to prevent the commit changed in the runc --version
+        # git add . && git commit -m "apply patch $line"
     fi
 done <"$series"
